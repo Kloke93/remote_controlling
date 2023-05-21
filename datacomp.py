b4730a836e8ee28ff8958398eb5cc2550ee21414
tomas dal farra <tomasdfarra@gmail.com>
2023-05-21 04:21:28 +0300

    compression organization final

--- datacomp.py
+++ datacomp.py
@@ -6,20 +6,21 @@ Description: Video encoder for live-streaming
 import ffmpeg
 from dataget import VideoGather
 
-# socket_url = "udp://127.0.0.1:5010"
-socket_url = "udp://172.16.11.198:5010"
-
 
 class StreamEncode:
-    """ Encoding video stream from rawvideo rgb24 to libx264 h264 and from stdin to stdout """
-    def __init__(self, width, height):
+    """ Encoding video stream from rawvideo rgb24 to libx264 h264 and from stdin to url """
+    standard_url = 'pipe:'
+
+    def __init__(self, width, height, url):
         """
         Gets settings for a ffmpeg subprocess to encode from rawvideo rgb24 to h264 for live-streaming
         :param width: video data width length
         :param height: video data height length
+        :param url: location of the encoded data destination
         """
         self.width = width
         self.height = height
+        self.url = url
         self.process = None
 
     def run_encoder(self):
@@ -27,42 +28,62 @@ class StreamEncode:
         self.process = (
             ffmpeg
             .input(
-                'pipe:', format='rawvideo', pix_fmt='rgb24',
+                StreamEncode.standard_url, format='rawvideo', pix_fmt='rgb24',
                 s=f'{self.width}x{self.height}'
             )
             .output(
-                'pipe:',                                                        # output to stdout
+                self.url,                                                       # output to url
                 codec='libx264', format='h264', pix_fmt='yuv420p',              # encoding format
                 tune='zerolatency', preset='ultrafast', crf='23',               # quality and speed to encode
             )
-            .run_async(pipe_stdin=True, pipe_stdout=True)
+            # if url is stdout so it opens the pipe
+            .run_async(pipe_stdin=True, pipe_stdout=(StreamEncode.standard_url == self.url))
         )
+
+    def write_stdin(self, data):
+        """
+         Writes data to stdin
+        :param data: input data for stdin
+        """
+        self.process.stdin.write(data)
+
+    def close(self):
+        """ Closes the ffmpeg process """
+        self.process.stdin.close()
+        if self.url == StreamEncode.standard_url:
+            self.process.stdout.close()
+        self.process.kill()
 
 
 class ScreenEncode(StreamEncode):
     """ Encoding screenshots stream from rawvideo rgb24 to libx264 h264 and from stdin to stdout """
 
-    def __init__(self):
+    def __init__(self, url):
         """
         Set settings for a ffmpeg subprocess to encode from rawvideo rgb24 to h264 for live-streaming
         Specifically made for screen sharing
+        :param url: location of the encoded frames destination
         """
         self.camera = VideoGather()
         width = self.camera.monitor['width']
         height = self.camera.monitor['height']
-        super().__init__(width, height)
+        super().__init__(width, height, url)
+
+    def capture(self):
+        """ Captures screen and sends it to ffmpeg through stdin """
+        self.write_stdin(self.camera.get_frame())
 
 
 def main():
-    encode = ScreenEncode()
+    socket_url = "udp://127.0.0.1:5010"
+    # socket_url = "udp://172.16.11.198:5010"
+    encode = ScreenEncode(socket_url)
     encode.run_encoder()
     try:
         while True:
-            process.stdin.write(
-                camera.get_frame()
-            )
+            encode.capture()
     finally:
-        process.stdin.close()
+        encode.close()
 
 
 if __name__ == "__main__":
